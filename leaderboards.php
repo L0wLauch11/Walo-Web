09f2517438479e90c0cccd83b465802759bdbc20
L0wLauch11 <42972587+L0wLauch11@users.noreply.github.com>
2022-07-30 20:47:51 +0200

    add page for minecraft plugin

--- leaderboards.php
+++ leaderboards.php
@@ -2,90 +2,82 @@
 <html lang="en" dir="ltr">
 
 <head>
-  <link rel="shortcut icon" type="assets/walo-small.png" href="favicon.ico" />
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/master.css">
-  <title>Walo</title>
+    <link rel="shortcut icon" type="assets/walo-small.png" href="favicon.ico" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/master.css">
+    <title>Walo</title>
 </head>
 
 <body>
+    <header>
+        <?php
+        $root = $_SERVER["DOCUMENT_ROOT"];
+        include "$root/navigation.html";
+        ?>
+    </header>
 
-  <header>
-    <nav>
-      <ul>
-        <li>
-          <h1>Walo</h1>
-        </li>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="leaderboards.php">Leaderboards</a></li>
-        <li><a target="_blank" href="https://discord.gg/3ZHhqeUG">Discord</a></li>
-        <li><a target="_blank" href="https://github.com/L0wLauch11">GitHub</a></li>
-      </ul>
-    </nav>
-  </header>
+    <p id="heading">Leaderboards</p>
 
-  <p id="next-walo">Leaderboards</p>
+    <div class="container">
+        <h1>Kills:</h1>
 
-  <div class="container">
-    <h1>Kills:</h1>
+        <?php
+        $ini_array = parse_ini_file("assets/credentials.ini");
 
-    <?php
-    $ini_array = parse_ini_file("assets/credentials.ini");
+        $servername = $ini_array['servername'];
+        $username = $ini_array['username'];
+        $password = $ini_array['password'];
+        $dbname = $ini_array['dbname'];
 
-    $servername = $ini_array['servername'];
-    $username = $ini_array['username'];
-    $password = $ini_array['password'];
-    $dbname = $ini_array['dbname'];
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-
-    $sql = "SELECT * FROM walo";
-    $result = $conn->query($sql);
-    $num = 0;
-    $entries = false;
-
-    if ($result->num_rows > 0) {
-      // generate kill leaderboard
-
-      echo "<div id='rankings'>";
-      $i = 0;
-      while ($row = $result->fetch_assoc()) {
-        $div_darker = "";
-        $div_darker_end = "";
-
-        if ($i % 2 == 0) {
-          $div_darker = "<div class='leaderboard-darker'>";
-          $div_darker_end = "</div>";
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
         }
-        $i++;
 
-        if ($row["KILLS"] > 0) {
-          $name = $row['NAME'];
-          $kills = $row['KILLS'];
+        $sql = "SELECT * FROM walo";
+        $result = $conn->query($sql);
+        $num = 0;
+        $entries = false;
 
-          echo "<div class='row'>$div_darker<div class='name'>$name</div><div class='score'>$kills</div><div class='kills-text'>Kills</div>$div_darker_end</div>";
+        if ($result->num_rows > 0) {
+            // generate kill leaderboard
 
-          $entries = true;
+            echo "<div id='rankings'>";
+            $i = 0;
+            while ($row = $result->fetch_assoc()) {
+                $div_darker = "";
+                $div_darker_end = "";
+
+                if ($i % 2 == 0) {
+                    $div_darker = "<div class='leaderboard-darker'>";
+                    $div_darker_end = "</div>";
+                }
+                $i++;
+
+                if ($row["KILLS"] > 0) {
+                    $name = $row['NAME'];
+                    $kills = $row['KILLS'];
+
+                    echo "<div class='row'>$div_darker<div class='name'>$name</div><div class='score'>$kills</div><div class='kills-text'>Kills</div>$div_darker_end</div>";
+
+                    $entries = true;
+                }
+            }
+            echo "</div>";
         }
-      }
-      echo "</div>";
-    }
 
-    if ($entries == false) {
-      echo "<p id='no-data'>Noch keine Einträge</p>";
-    }
+        if ($entries == false) {
+            echo "<p id='no-data'>Noch keine Einträge</p>";
+        }
 
-    $conn->close();
-    ?>
+        $conn->close();
+        ?>
 
 
-  </div>
+    </div>
 </body>
 
 </html>
---
+++ plugin.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <link rel="shortcut icon" type="assets/walo-small.png" href="favicon.ico" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/master.css">
+    <title>Walo</title>
+</head>
+
+<body>
+    <header>
+        <?php
+        $root = $_SERVER["DOCUMENT_ROOT"];
+        include "$root/navigation.html";
+        ?>
+    </header>
+
+    <p id="heading">Walo Minecraft-Plugin</p>
+
+    <div class="container">
+        <p id="info-text">Neuste Version:</p>
+        <div class="info">
+            <ul>
+                <?php
+
+                $files = glob('walo-plugin/*.jar');
+                usort($files, function($a, $b) {
+                    return filemtime($b) - filemtime($a);
+                });
+                
+                $counter = 0;
+                foreach($files as $file) {
+                    if($counter == 1) {
+                        print '<p id="info-text">Ältere Versionen:</p>';
+                    }
+
+                    $filename = basename($file);
+                    print "<li><a href='walo-plugin/$filename'>$filename</a>";
+
+                    if (file_exists("walo-plugin/$filename.html"))
+                        include "walo-plugin/$filename.html";
+
+                    print "</li>";
+
+                    $counter++;
+                }
+
+                ?>
+            </ul>
+        </div>
+
+        <p id="web-source">Plugin Quellcode: <a
+                href="https://github.com/L0wLauch11/Walo">https://github.com/L0wLauch11/Walo</a></p>
+    </div>
+
+</body>
+
+</html>
