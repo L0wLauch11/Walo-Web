69682ccea65142d0d57d3879db1d6d61c8cdfd43
L0wLauch11 <42972587+L0wLauch11@users.noreply.github.com>
2022-08-07 15:08:14 +0200

    create database api for walo mc plugin

---
+++ database.php
@@ -0,0 +1,100 @@
+<?php
+$minecraft_uuid = $_GET['uuid'];
+$minecraft_name = $_GET['name'];
+$security_string = $_GET['secret'];
+$operation = $_GET['operation'];
+
+if (!isset($minecraft_uuid) || !isset($minecraft_name) || !isset($security_string) || !isset($operation)) {
+    echo 'Please make sure you input a minecraft player uuid the security string and an operation';
+    return;
+}
+
+if ($security_string != file_get_contents('assets/secrets/database_access_security_string.txt')) {
+    echo 'Wrong security string';
+    return;
+}
+
+$ini_array = parse_ini_file("assets/secrets/credentials.ini");
+
+$servername = $ini_array['servername'];
+$username = $ini_array['username'];
+$password = $ini_array['password'];
+$dbname = $ini_array['dbname'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = '';
+switch ($operation) {
+    case 'inittable':
+        $sql = 'CREATE TABLE IF NOT EXISTS walo (UUID VARCHAR(100), NAME VARCHAR(100), KILLS INT(100))';
+        $conn->query($sql);
+
+        break;
+
+    case 'createplayer':
+        try {
+            $sql_get_kills = "SELECT * FROM walo WHERE UUID='$minecraft_uuid'";
+            $result = $conn->query($sql_get_kills);
+        } catch (Exception $e) {
+            // This exception means that the player doesn't exist yet
+
+            $sql = "INSERT INTO walo (UUID, NAME, KILLS) VALUES ('$minecraft_uuid', '$minecraft_name', 0)";
+            $conn->query($sql);
+        }
+
+        break;
+
+    case 'addkill':
+        $kills = 0;
+        $sql_get_kills = "SELECT * FROM walo WHERE UUID='$minecraft_uuid'";
+
+        $result = $conn->query($sql_get_kills);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $kills = $row['KILLS'];
+            }
+        }
+
+        $new_kills = $kills + 1;
+        $sql = "UPDATE walo SET KILLS=$new_kills WHERE UUID='$minecraft_uuid'";
+        $conn->query($sql);
+
+        // Sort table by highest kill amount
+        $sql = "ALTER TABLE walo ORDER BY KILLS DESC";
+        $conn->query($sql);
+
+        break;
+    
+    case 'getkills':
+        $kills = 0;
+        $sql_get_kills = "SELECT * FROM walo WHERE UUID='$minecraft_uuid'";
+
+        $result = $conn->query($sql_get_kills);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $kills = $row['KILLS'];
+            }
+        }
+
+        echo $kills;
+
+        break;
+
+    default:
+        echo 'Not a valid operation';
+    
+        break;
+}
+
+if ($sql == '') {
+    return;
+}
+
+$conn->close();
+?>
