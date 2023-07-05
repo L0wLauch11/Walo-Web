ac7e272d8e09fd687670ec700cc7d1c9e65c1629
L0wLauch11 <42972587+L0wLauch11@users.noreply.github.com>
2023-07-05 21:09:29 +0200

    remove unused script

--- leaderboards.php
+++ leaderboards.php
@@ -142,23 +142,6 @@
         $conn->close();
         ?>
     </div>
-
-    <script>
-        var coll = document.getElementsByClassName("collapsible");
-        var i;
-
-        for (i = 0; i < coll.length; i++) {
-            coll[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                var content = this.nextElementSibling;
-                if (content.style.display !== "none") {
-                    content.style.display = "none";
-                } else {
-                    content.style.display = "block";
-                }
-            });
-        }
-    </script>
 </body>
 
 </html>
