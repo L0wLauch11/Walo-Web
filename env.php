4d6b4409fb70b79e7f30c47e855d556989d5a292
L0wLauch11 <42972587+L0wLauch11@users.noreply.github.com>
2024-09-11 15:26:40 +0200

    Delete env.php
    you should now modify env.example.php

--- env.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Env {
-    public static $mc_server_address = "mc-walo.xyz"; // Change to whatever your minecraft-servers address is
-    public static $website_name = "MC-Walo"; // Whatever you want to call this website
-    public static $contact_address = "lowlauch@proton.me"; // The E-Mail address where users can contact you
-}
