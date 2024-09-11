675ac76f54f87e7b572efb4177f526e50abec804
L0wLauch11 <42972587+L0wLauch11@users.noreply.github.com>
2024-09-11 15:26:01 +0200

    Update env.php

--- env.php
+++ env.php
@@ -1,5 +1,7 @@
 <?php
 
 class Env {
-    public static $server_address = "mc-walo.xyz";
+    public static $mc_server_address = "mc-walo.xyz"; // Change to whatever your minecraft-servers address is
+    public static $website_name = "MC-Walo"; // Whatever you want to call this website
+    public static $contact_address = "lowlauch@proton.me"; // The E-Mail address where users can contact you
 }
